eeb685332c41babafe71be95c0e55f0d22643194
Michael C. Main <deusprogrammer@gmail.com>
2021-06-06 05:44:27 -0500

    React is being a bitch again

--- src/utils/ApiHelper.js
+++ src/utils/ApiHelper.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const YT_REQ_URL="https://deusprogrammer.com/api/yt";
+
 const getOptions = () => {
     return {
         headers: {
@@ -9,28 +11,28 @@ const getOptions = () => {
 }
 
 export const gameSearch = async (text, page) => {
-    let res = await axios.get(`${process.env.REACT_APP_YT_REQ_URL}/games?search=${text}&page=${page}&pageSize=10`);
+    let res = await axios.get(`${YT_REQ_URL}/games?search=${text}&page=${page}&pageSize=10`);
     return res.data;
 }
 
 export const getGame = async (gameId) => {
-    let res = await axios.get(`${process.env.REACT_APP_YT_REQ_URL}/games/${gameId}`);
+    let res = await axios.get(`${YT_REQ_URL}/games/${gameId}`);
     return res.data;
 }
 
 export const getUsers = async () => {
-    let res = await axios.get(`${process.env.REACT_APP_YT_REQ_URL}/users`);
+    let res = await axios.get(`${YT_REQ_URL}/users`);
     return res.data;
 }
 
 export const getUser = async (id) => {
-    let res = await axios.get(`${process.env.REACT_APP_YT_REQ_URL}/users/${id}`);
+    let res = await axios.get(`${YT_REQ_URL}/users/${id}`);
     return res.data;
 }
 
 export const getMyUser = async () => {
     try {
-        let res = await axios.get(`${process.env.REACT_APP_YT_REQ_URL}/me`, getOptions());
+        let res = await axios.get(`${YT_REQ_URL}/me`, getOptions());
         return res.data;
     } catch (error) {
         // If jwt expired, then store the new jwt and retry.
@@ -44,7 +46,7 @@ export const getMyUser = async () => {
 
 export const createChannel = async (name) => {
     try {
-        let res = await axios.post(`${process.env.REACT_APP_YT_REQ_URL}/channels`, {
+        let res = await axios.post(`${YT_REQ_URL}/channels`, {
             name
         }, getOptions());
         return res.data;
@@ -60,7 +62,7 @@ export const createChannel = async (name) => {
 
 export const getChannels = async () => {
     try {
-        let res = await axios.get(`${process.env.REACT_APP_YT_REQ_URL}/channels`, getOptions());
+        let res = await axios.get(`${YT_REQ_URL}/channels`, getOptions());
         return res.data;
     } catch (error) {
         // If jwt expired, then store the new jwt and retry.
@@ -74,7 +76,7 @@ export const getChannels = async () => {
 
 export const getChannel = async (channelId) => {
     try {
-        let res = await axios.get(`${process.env.REACT_APP_YT_REQ_URL}/channels/${channelId}`, getOptions());
+        let res = await axios.get(`${YT_REQ_URL}/channels/${channelId}`, getOptions());
         return res.data;
     } catch (error) {
         // If jwt expired, then store the new jwt and retry.
@@ -88,7 +90,7 @@ export const getChannel = async (channelId) => {
 
 export const updateChannel = async (id, channelData) => {
     try {
-        let res = await axios.put(`${process.env.REACT_APP_YT_REQ_URL}/channels/${id}`, channelData, getOptions());
+        let res = await axios.put(`${YT_REQ_URL}/channels/${id}`, channelData, getOptions());
         return res.data;
     } catch (error) {
         // If jwt expired, then store the new jwt and retry.
@@ -102,7 +104,7 @@ export const updateChannel = async (id, channelData) => {
 
 export const updateRequests = async (id, requests) => {
     try {
-        let res = await axios.put(`${process.env.REACT_APP_YT_REQ_URL}/channels/${id}/requests`, requests, getOptions());
+        let res = await axios.put(`${YT_REQ_URL}/channels/${id}/requests`, requests, getOptions());
         return res.data;
     } catch (error) {
         // If jwt expired, then store the new jwt and retry.
